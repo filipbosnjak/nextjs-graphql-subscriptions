1f0d69488b3c230bd6f746be688aad62107937ca
Filip <filipo.bosnjak@gmail.com>
2024-02-05 17:19:09 +0100

    ci/cd

--- components/client-components/Info.tsx
+++ components/client-components/Info.tsx
@@ -13,7 +13,7 @@ import {Button} from "@/components/ui/button";
 import {gqlClient} from "@/components/GraphqlClient";
 import {useSSESubscription} from "@/client-ops/Subscriptions";
 
-export const base = process.env.NODE_ENV === "development" ? 'http://localhost:3000': 'https://nextjs-graphql-subscriptions.vercel.app/' ;
+export const base = process.env.NODE_ENV === "development" ? 'http://localhost:3000': 'https://nextgql-bqkhkcvura-uc.a.run.app/' ;
 
 
 export type InfoProps = {
