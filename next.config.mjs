61f66fdf74162c965f636651c61a2a212eb6a474
Filip <filipo.bosnjak@gmail.com>
2024-02-05 12:23:23 +0100

    cors update

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,27 @@ const nextConfig = {
     appDir: true,
   },
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Accept, Origin, Authorization',
+          },
+        ],
+      },
+    ]
+  }
 }
 
 export default nextConfig
